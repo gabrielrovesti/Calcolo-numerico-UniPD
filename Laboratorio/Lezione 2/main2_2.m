5d03280b58c31315b4040fa83a567a2df7532d32
Gabriel Rovesti <95100089+gabrielrovesti@users.noreply.github.com>
2022-04-13 23:01:18 +0200

    Add files via upload

---
+++ Laboratorio/Lezione 2/main2_2.m
@@ -0,0 +1,8 @@
+clear all;
+a = input("Inserisci a: ");
+b = input("Inserisci b: ");
+c = input("Inserisci c: ");
+x1_vera = input("Inserisci x1 vera: ");
+x2_vera = input("Inserisci x2 vera: ");
+
+%%Riprendere qui
