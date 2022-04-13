94f19da389097f11b386f126f0669a2b05c065ae
Gabriel Rovesti <95100089+gabrielrovesti@users.noreply.github.com>
2022-04-13 22:39:19 +0200

    Add files via upload

--- Laboratorio/Lezione 2/main2_1a.m
+++ Laboratorio/Lezione 2/main2_1a.m
@@ -1,4 +1,18 @@
 clear all;
-
-eq2gr
+%%Pulisco lo script e vado agli input classici
+a = input("Inserisci a: ");
+b = input("Inserisci b: ");
+c = input("Inserisci c: ");
+x1_vera = input("Inserisci x1 vera: ");
+x2_vera = input("Inserisci x2 vera: ");
+if(a ~= 0 && b ~= 0 && c ~= 0)
+    eq2gr;  %%Richiamo normalmente lo script
+    rel1 = abs(x1_vera - x1) / abs(x1_vera);
+    rel2 = abs(x2_vera - x2) / abs(x2_vera);
+    %%Calcolo gli errori relativi, con stampa alla c
+    fprintf("Errore relativo x1 = %d\n", rel1);
+    fprintf("Errore relativo x2 = %d\n", rel2);
+else
+    error("Inseriti valori nulli");
+end
 
