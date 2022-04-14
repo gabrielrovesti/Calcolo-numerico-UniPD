e1ecccd79b2d08ffdc68da4691827ea9d92a8fa4
Gabriel Rovesti <95100089+gabrielrovesti@users.noreply.github.com>
2022-04-14 22:51:53 +0200

    Add files via upload

--- Laboratorio/Lezione 2/main2_2.m
+++ Laboratorio/Lezione 2/main2_2.m
@@ -5,4 +5,10 @@ c = input("Inserisci c: ");
 x1_vera = input("Inserisci x1 vera: ");
 x2_vera = input("Inserisci x2 vera: ");
 
-%%Riprendere qui
+eq2grstab_all
+
+rel1=abs(x1_vera - x1)/abs(x1_vera);
+rel2=abs(x2_vera - x2)/abs(x2_vera);
+fprintf("Errore relativo x1: %d\n", rel1);
+fprintf("Errore relativo x1: %d\n", rel2);
+
