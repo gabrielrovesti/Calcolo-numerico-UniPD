b496c16f9c6da578b5b2b18876a46d60984dbc4b
Gabriel Rovesti <95100089+gabrielrovesti@users.noreply.github.com>
2022-05-08 15:23:17 +0200

    Delete Laboratorio/Lezione 3 directory

--- Laboratorio/Lezione 3/Esercizio_1.m
+++
@@ -1,8 +0,0 @@
-clear all
-x=linspace(0,1); %100 è già implicito
-f=@(x) exp(x).*(x.^2+1);
-z=f(x);
-y=g(x);
-
-plot(x,y,x,z);
-title('Figura 10: Funzioni z ed y');
--- Laboratorio/Lezione 3/g.m
+++
@@ -1,4 +0,0 @@
-%vado a dare in output la funzione g(x)
-function [out] = g(x)
-    out = exp(x)./(x.^2 + 1);
-end
